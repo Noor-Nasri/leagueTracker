05313593a3b34a65aeb72c7e86c65362e44cdd7c
Noor-Nasri <noor.nasri@hotmail.com>
2023-02-09 15:52:26 -0500

    Firebase setup complete

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import pyrebase
-
-config = {
-  "apiKey": "apiKey",
-  "authDomain": "projectId.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "projectId.appspot.com"
-}
-
-
+from pyrebase import pyrebase
+from keys import firebase_config
 
 if __name__ == "__main__":
-    firebase = pyrebase.initialize_app(config)
-    print("Hello world")
+    firebase = pyrebase.initialize_app(firebase_config)
+    db = firebase.database()
+    #data = {"name": "Mortimer 'Morty' Smith"}
+    #db.child("users").push(data)
+
+    print("Done")
