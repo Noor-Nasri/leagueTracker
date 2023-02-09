73c4a6bb62f342048a19651006d102037118b782
Noor-Nasri <noor.nasri@hotmail.com>
2023-02-09 17:49:08 -0500

    Main logic setup

--- main.py
+++ main.py
@@ -2,14 +2,40 @@ from pyrebase import pyrebase
 from keys import firebase_config, discord_token
 import discord
 from discord.ext import commands, tasks
+from data import champion_pool
+from typing import List
 
-class Commands(commands.Cog):
-    def __init__(self, client):
-        self.client = client
-        
-    @commands.command(help = "[username] creates a new folder for this user")
-    async def add_player(self, ctx, arg):
-        await ctx.channel.send("WIP")
+def add_player(nickname: str, discord_tag:str) -> bool:
+    # Creates a new folder for the player and links it to the discord tag
+    # True on success and False when nickname in use OR invalid tag
+
+    pass
+
+def ban_champ(name: str) -> bool:
+    # Returns true on success, false on champ not found
+
+    pass
+
+def unban_champ(name: str) -> bool:
+    # Returns true on success, false on champ not found
+
+    pass
+
+def get_banlist() -> List[str]:
+    # Returns a list of champion names
+
+    pass
+
+async def generate_matchup(ctx): 
+    # Still figuring out return
+
+    if not (ctx.author.voice and ctx.author.voice.channel):
+        await ctx.send("You are not connected to a voice channel")
+        return 
+
+    channel = ctx.author.voice.channel
+
+    await ctx.channel.send("WIP")
 
 if __name__ == "__main__":
     # connect to firebase and initialize discord bot
