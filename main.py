614101561e504caf65e2824972bb80aa5f6cca3a
Noor-Nasri <noor.nasri@hotmail.com>
2023-02-09 19:52:23 -0500

    Matchmaking

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import discord
 from discord.ext import commands, tasks
 from data import champion_pool
 from typing import List, Dict
+from random import shuffle
 
 def sign_up(ctx, nickname: str) -> bool:
     # Creates a new folder for the player and links it to the discord tag
     # returns false if it already exists
 
     all_player_ids = db.child("users").shallow().get()
-    player_id = str(ctx.message.author.id)
+    player_id = ctx.message.author.id
     if player_id in all_player_ids.val():
         return False
 
@@ -55,18 +56,42 @@ def get_banlist(db) -> List[str]:
     return db.child("banlist").get().val()[1:]
 
 
-def generate_matchup(ctx) -> List[Dict[str, str]]:
+def generate_matchup(db, ctx) -> List[Dict[str, str]]:
     # Generates a valid matchup and returns it in [{"ziadom" : "Yorick"}, {team 2}]
-    # If -1 is returned, display "ERROR: You are not connected to a voice channel"
+    # If -1 is returned, display "You are not connected to a voice channel"
     # If -2 is returned, display "Uneven number of players in voice channel"
+    # If -3 is returned, display "No league players found in vc"
 
     if not (ctx.author.voice and ctx.author.voice.channel):
-        return []
+        return -1
 
     channel = ctx.author.voice.channel
     member_ids = channel.voice_states.keys()
-    print(member_ids)
-    return []
+    all_player_ids = db.child("users").shallow().get()
+    player_ids = []
+    for member_id in member_ids:
+        if member_id in all_player_ids:
+            player_ids.append(member_id)
+    
+    if not player_ids: return -3
+    if len(player_ids) % 2 != 0: return -2
+
+    teams = [{}, {}]
+    banlist = get_banlist(db)
+    champs = champion_pool[:]
+    shuffle(champs)
+    shuffle(player_ids)
+
+    for i in range(len(player_ids)):
+        username = db.child("users").child(player_ids[i]).get().val().get("name")
+        while champs[i] in banlist:
+            champs.pop(i)
+
+        teams[i % 2][username] = champs[i] 
+        banlist.append(champs[i])
+
+    print(teams)
+    return teams
 
 def accept_matchup(teams: List[Dict[str, str]]) -> int:
     # Saves the matchup and remembers the integer 
@@ -82,8 +107,10 @@ if __name__ == "__main__":
     firebase = pyrebase.initialize_app(firebase_config)
     db = firebase.database()
 
-    print(unban_champ(db, "zoe"))
+    #print(db.child("users").child("1981").get().val().get("name"))
     #db.child("banlist").set(["Init"])
+    #db.child("users").set({0 : {"name": "Default", "wins": 0, "losses": 0}})
+    #db.child("matches").set([{"Team 0": ["Default"], "Team 1": ["Default"], "Winner": 1}])
 
     """
     client = commands.Bot(command_prefix="!")
