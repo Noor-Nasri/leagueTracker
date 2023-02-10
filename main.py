c5701091306c020ed177dc860849438c91f98a0c
Noor-Nasri <noor.nasri@hotmail.com>
2023-02-09 20:27:11 -0500

    Added match conclusions

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from data import champion_pool
 from typing import List, Dict
 from random import shuffle
 
-def sign_up(ctx, nickname: str) -> bool:
+def sign_up(author, nickname: str) -> bool:
     # Creates a new folder for the player and links it to the discord tag
     # returns false if it already exists
 
-    all_player_ids = db.child("users").shallow().get()
-    player_id = ctx.message.author.id
-    if player_id in all_player_ids.val():
+    all_player_ids = db.child("users").shallow().get().val()
+    if str(author.id) in all_player_ids:
         return False
 
     data = {"name": nickname, "wins": 0, "losses": 0}
-    db.child("users").child(player_id).set(data)
+    db.child("users").child(author.id).set(data)
     return True
 
 
@@ -56,18 +55,19 @@ def get_banlist(db) -> List[str]:
     return db.child("banlist").get().val()[1:]
 
 
-def generate_matchup(db, ctx) -> List[Dict[str, str]]:
-    # Generates a valid matchup and returns it in [{"ziadom" : "Yorick"}, {team 2}]
+def generate_matchup(db, author) -> List[Dict[str, str]]:
+    # Generates a valid matchup and returns it in [{"ziadom" : "Yorick"}, 
+    # {team 2}, {"ziadom" : "_id_"}]
     # If -1 is returned, display "You are not connected to a voice channel"
     # If -2 is returned, display "Uneven number of players in voice channel"
     # If -3 is returned, display "No league players found in vc"
 
-    if not (ctx.author.voice and ctx.author.voice.channel):
+    if not (author.voice and author.voice.channel):
         return -1
 
-    channel = ctx.author.voice.channel
+    channel = author.voice.channel
     member_ids = channel.voice_states.keys()
-    all_player_ids = db.child("users").shallow().get()
+    all_player_ids = db.child("users").shallow().get().val()
     player_ids = []
     for member_id in member_ids:
         if member_id in all_player_ids:
@@ -95,11 +95,25 @@ def generate_matchup(db, ctx) -> List[Dict[str, str]]:
 
 def accept_matchup(teams: List[Dict[str, str]]) -> int:
     # Saves the matchup and remembers the integer 
-    pass
+
+    num_matches = len(db.child("matches").shallow().get().val())
+    data = {"Team 0": [teams[2][e] for e in teams[0].keys()], 
+            "Team 1": [teams[2][e] for e in teams[1].keys()], 
+            "Winner": -1}
+    
+    db.child("matches").child(num_matches).set(data)
+    return num_matches
 
 def conclude_match(match_id: int, winner_index: int) -> bool:
     # Saves winner of the match, returns false if id or index is invalid
-    pass
+
+    matches = db.child("matches").shallow().get().val()
+    if not str(match_id) in db.child("matches").shallow().get().val() or \
+        winner_index != 0 and winner_index != 1:
+        return False
+    
+    db.child("matches").child(match_id).update({"Winner": winner_index})
+    return False
     
 
 if __name__ == "__main__":
@@ -112,6 +126,14 @@ if __name__ == "__main__":
     #db.child("users").set({0 : {"name": "Default", "wins": 0, "losses": 0}})
     #db.child("matches").set([{"Team 0": ["Default"], "Team 1": ["Default"], "Winner": 1}])
 
+    #db.child("users").child(0).update({"wins": 2})
+    
+    #print(accept_matchup([
+    #    {"Ziadom": "Nunu"}, 
+    #    {"Pop": "Yorick"}, 
+    #    {"Ziadom": 1022, "Pop": 1829}]))
+    
+    #print(conclude_match(1, 0))
     """
     client = commands.Bot(command_prefix="!")
 
